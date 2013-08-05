2520743b2f08ef5e7ab71bb905fb79dadca74c28
StoyanD <s.dimitrov19@gmail.com>
2013-08-05 10:32:41 -0700

    updated functions and cleaned up code

--- src/com/bithedge/scriptroot/MainActivity.java
+++ src/com/bithedge/scriptroot/MainActivity.java
@@ -19,43 +19,73 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+/**
+ * A activity that executes a file as root.
+ * <p>
+ * @author Stoyan Dimitrov
+ */
 public class MainActivity extends Activity {
 	private static final String TAG = "MainActivity";
 
 	public static final String SH_LABEL = ".sh";
 
-	public static final String EXC_LABEL = "./";
-
+	public static final String EXC_LABEL = "sh ";
+	/**
+	 * 	start here http://stackoverflow.com/questions/9386972/interactive-commands-using-java-runtime-getruntime-exec
+     *	remember http://stackoverflow.com/questions/11822500/changing-system-file-permission
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		String filename = getResources().getResourceEntryName(R.raw.dump_reg) + SH_LABEL;
+		String filename = getResources().getResourceEntryName(R.raw.simple) + SH_LABEL;
 		Log.i(TAG, "filename :  " + filename);
-		saveScript(filename, loadFile(R.raw.dump_reg));
-		setRoot2(filename);
-//		setBlazeRoot(filename);
-//		setRoot(saveScript(loadFile()));
-//		setRoot(loadFile());
-
+		File file = saveScript(filename, loadFile(R.raw.simple));
+		Log.i(TAG, "file dir : " + file.getParent());
+		initButtons(file);
 	}
-	private String saveScript(String filename, String content){
+
+	/**
+	 * Initialize the one button in the activity to execute the file as root
+	 * @param file The file to execute as root with {@link #execRoot(File)}
+	 */
+	private void initButtons(final File file){
+		Button b = (Button)findViewById(R.id.rootButton);
+
+        b.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+            	execRoot(file);
+            }
+        });
+	}
+	/**
+	 * Saves a file into the package/files directory
+	 * <p>
+	 * @param filename The name of the file including the file extension type, ie. .txt, .sh,...
+	 * @param content The content of the file
+	 * @return The saved file
+	 */
+	private File saveScript(String filename, String content){
 		FileOutputStream outputStream;
 		File file = new File(this.getFilesDir(), filename);
 
-
 		try {
-//		  outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-		  outputStream = openFileOutput(file.getName(), Context.MODE_WORLD_READABLE);
+		  outputStream = openFileOutput(file.getName(), Context.MODE_PRIVATE);
 		  outputStream.write(content.getBytes());
 		  outputStream.close();
-		  return file.getAbsolutePath();
+		  return file;
 		} catch (Exception e) {
 		  e.printStackTrace();
 		}
 		return null;
 	}
 
+	/**
+	 * Loads a file from the res/raw directory and reads its contents
+	 * @param res_id The resource ID of the res/raw file
+	 * @return The string contents of the file
+	 */
 	private String loadFile(int res_id){
 		InputStream databaseInputStream = getResources().openRawResource(res_id);
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(databaseInputStream));
@@ -66,74 +96,71 @@ public class MainActivity extends Activity {
 				sb.append(line);
 				sb.append("\n");
 			}
-//			Log.i(TAG, "file : " + sb.toString());
 			return sb.toString();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	private void setRoot2(final String filename){
-		Button b = (Button)findViewById(R.id.rootButton);
+	/**
+	 * Executes a file as root. Changes file permissions to 744 -rwxr--r--
+	 * @param file The file to be executed as root
+	 */
+	private void execRoot(final File file){
+		Process proc = null;
 
+    	try {
+    	   proc = Runtime.getRuntime().exec("su", null, file.getParentFile());
+    	}catch (IOException e) {
+    	   e.printStackTrace();
+    	}
+    	if (proc != null) {
+    	   BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+    	   PrintWriter out = new PrintWriter(proc.getOutputStream(), true);
+    	   out.println("pwd");
+    	   out.println("ls -l");
+    	   out.println("chmod 744 " + file);
+    	   out.println("ls -l");
+    	   out.println(EXC_LABEL + file.getName());
+    	   out.println("exit");
+    	   try {
+    	       String line;
+    	       while ((line = in.readLine()) != null) {
+    	    	   Log.e(TAG, line);
+    	       }
+    	       proc.waitFor();
+    	       in.close();
+    	       out.close();
+    	       proc.destroy();
 
-        b.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-            	Process proc = null;
-            	try {
-            	   proc = Runtime.getRuntime().exec("su", null, new File("/data/data/com.bithedge.scriptroot/files/"));
-            	}catch (IOException e) {
-            	   e.printStackTrace();
-            	}
-            	if (proc != null) {
-            	   BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            	   PrintWriter out = new PrintWriter(proc.getOutputStream(), true);
-            	   out.println("pwd");
-            	   out.println("ls -l");
-            	   out.println("chmod 744 " + filename);
-            	   out.println("ls -l");
-            	   out.println(EXC_LABEL + filename);
-            	   out.println("exit");
-            	   try {
-//            		   Log.e(TAG, "try read");
-            	       String line;
-            	       while ((line = in.readLine()) != null) {
-            	    	   Log.e(TAG, line);
-            	       }
-            	       proc.waitFor();
-            	       in.close();
-            	       out.close();
-            	       proc.destroy();
+    	       if(proc.exitValue() == 1){
+    	    	   Log.w(TAG, "proc.exitValue() : 	" + proc.exitValue());
+    	    	   Log.w(TAG, "execRoot fail, starting execBlazeRoot");
+    	    	   execBlazeRoot(file);
+    	       }
+    	   }catch (Exception e) {
+    		   e.printStackTrace();
 
-            	       if(proc.exitValue() == 1){
-            	    	   Log.w(TAG, "proc.exitValue() : 	" + proc.exitValue());
-            	    	   Log.w(TAG, "setRoot2 fail, starting setBlazeRoot");
-            	    	   setBlazeRoot(filename);
-            	       }
-            	   }catch (Exception e) {
-            		   e.printStackTrace();
-
-            	   }
-            	}
-            }
-
-        });
+    	   }
+    	}
 	}
 
-	private void setBlazeRoot(final String filename){
+	/**
+	 * Execute a file as root on a device that has a process that cannot be writen to after creation, ie. Blaze tablet.
+	 * Changes file permissions to 744 -rwxr--r--
+	 * <p>
+	 * @param file the file to execute as root
+	 */
+	private void execBlazeRoot(final File file){
 		Process proc = null;
 		BufferedReader in = null;
  	    PrintWriter out = null;
 		try {
-//	    	   Log.e(TAG, "trying execution again");
-	    	   proc = Runtime.getRuntime().exec("chmod 744 " + filename, null, new File("/data/data/com.bithedge.scriptroot/files/"));
-	    	   proc = Runtime.getRuntime().exec("ls", null, new File("/data/data/com.bithedge.scriptroot/files/"));
-	    	   Log.i(TAG, "executing file  :" + filename);
-//		   	   proc = Runtime.getRuntime().exec(EXC_LABEL + filename, null, new File("/data/data/com.bithedge.scriptroot/files/"));
-	    	   proc = Runtime.getRuntime().exec("sh " + filename, null, new File("/data/data/com.bithedge.scriptroot/files/"));
+	    	   proc = Runtime.getRuntime().exec("chmod 744 " + file, null, file.getParentFile());
+	    	   proc = Runtime.getRuntime().exec("ls", null, file.getParentFile());
+	    	   Log.i(TAG, "executing file : " + file.getName());
+	    	   proc = Runtime.getRuntime().exec(EXC_LABEL + file.getName(), null, file.getParentFile());
 		       in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 		       out = new PrintWriter(proc.getOutputStream(), true);
 		       String line;
@@ -143,87 +170,12 @@ public class MainActivity extends Activity {
 	  	       proc.waitFor();
 	  	       in.close();
 	  	       out.close();
-//	  	       proc.destroy();
+	  	       proc.destroy();//TODO Blaze fails to destroy process
 		 }catch (IOException e1) {
-				// TODO Auto-generated catch block
 		    	e1.printStackTrace();
 		 } catch (InterruptedException e1) {
-				// TODO Auto-generated catch block
 		  		e1.printStackTrace();
 		 }
-	}
-	/**
-	 * 	start here http://stackoverflow.com/questions/9386972/interactive-commands-using-java-runtime-getruntime-exec
-     *	remember http://stackoverflow.com/questions/11822500/changing-system-file-permission
-	 */
-
-	private void setRoot(final String script){
-		Button b = (Button)findViewById(R.id.rootButton);
-
-
-        b.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Process process = null;
-                BufferedReader bufferedReader = null;
-                try {
-//                	Log.e(TAG, "path : " + script);
-//                	process = Runtime.getRuntime().exec("su -c " + script);
-//                	process = Runtime.getRuntime().exec("su -c /data/data/com.bithedge.scriptroot/files/my.sh");
-//                	process = Runtime.getRuntime().exec("su -c /data/data/com.bithedge.scriptroot/files/my.sh");
-//                    process = new ProcessBuilder()
-//                    .command("/data/scripts/simple.sh")
-//                    .command(script)
-//                    .start();
-//                	 process = Runtime.getRuntime().exec("su", null, new File("/system/bin/"));
-                	 process = Runtime.getRuntime().exec(new String[] {"chmod 777 /data/data/com.bithedge.scriptroot/files/my.sh;","./data/data/com.bithedge.scriptroot/files/my.sh"});
-                     OutputStream os = process.getOutputStream();
-
-
-//                     os.write(("chmod 777 /data/data/com.bithedge.scriptroot/files/my.sh;").getBytes("ASCII"));
-//                     os.flush();
-//                     os.close();
-                     try {
-                    	 process.waitFor();
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-//                     os = process.getOutputStream();
-//                     os.write(("sh /data/data/com.bithedge.scriptroot/files/my.sh").getBytes("ASCII"));
-//                     os.flush();
-//                     os.close();
-//                     try {
-//                    	 process.waitFor();
-//					} catch (InterruptedException e) {
-//						// TODO Auto-generated catch block
-//						e.printStackTrace();
-//					}
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-//                    if(p!=null) p.destroy();
-                	if(process !=  null){
-                		final StringBuilder log = new StringBuilder();
-                		bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                		String line;
-                        try {
-                        	if(bufferedReader != null){
-                        		while ((line = bufferedReader.readLine()) != null){
-    							    log.append(line);
-    							    log.append("\n");
-    							}
-                        	}
-
-							Log.e(TAG,"output : " + log.toString());
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-                	}
-                }
-            }
-        });
 	}
 
 	@Override
