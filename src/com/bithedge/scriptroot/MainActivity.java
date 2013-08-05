9bbdffafe83f84024cb2db761983914834b5d483
StoyanD <s.dimitrov19@gmail.com>
2013-08-05 09:46:43 -0700

    comitting changes of working camera register dump from script, ignoring /raw folder of bash scripts

--- src/com/bithedge/scriptroot/MainActivity.java
+++ src/com/bithedge/scriptroot/MainActivity.java
@@ -22,18 +22,25 @@ import android.widget.Button;
 public class MainActivity extends Activity {
 	private static final String TAG = "MainActivity";
 
+	public static final String SH_LABEL = ".sh";
+
+	public static final String EXC_LABEL = "./";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		saveScript(loadFile());
-		setRoot2();
+		String filename = getResources().getResourceEntryName(R.raw.dump_reg) + SH_LABEL;
+		Log.i(TAG, "filename :  " + filename);
+		saveScript(filename, loadFile(R.raw.dump_reg));
+		setRoot2(filename);
+//		setBlazeRoot(filename);
 //		setRoot(saveScript(loadFile()));
 //		setRoot(loadFile());
+
 	}
-	private String saveScript(String content){
+	private String saveScript(String filename, String content){
 		FileOutputStream outputStream;
-		String filename = "my.sh";
 		File file = new File(this.getFilesDir(), filename);
 
 
@@ -49,8 +56,8 @@ public class MainActivity extends Activity {
 		return null;
 	}
 
-	private String loadFile(){
-		InputStream databaseInputStream = getResources().openRawResource(R.raw.simple);
+	private String loadFile(int res_id){
+		InputStream databaseInputStream = getResources().openRawResource(res_id);
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(databaseInputStream));
 		String line;
 		final StringBuilder sb = new StringBuilder();
@@ -68,7 +75,7 @@ public class MainActivity extends Activity {
 		return null;
 	}
 
-	private void setRoot2(){
+	private void setRoot2(final String filename){
 		Button b = (Button)findViewById(R.id.rootButton);
 
 
@@ -78,34 +85,72 @@ public class MainActivity extends Activity {
             	Process proc = null;
             	try {
             	   proc = Runtime.getRuntime().exec("su", null, new File("/data/data/com.bithedge.scriptroot/files/"));
-            	}
-            	catch (IOException e) {
+            	}catch (IOException e) {
             	   e.printStackTrace();
             	}
             	if (proc != null) {
             	   BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
             	   PrintWriter out = new PrintWriter(proc.getOutputStream(), true);
             	   out.println("pwd");
-            	   out.println("cd ..");
-            	   out.println("pwd");
+            	   out.println("ls -l");
+            	   out.println("chmod 744 " + filename);
+            	   out.println("ls -l");
+            	   out.println(EXC_LABEL + filename);
             	   out.println("exit");
             	   try {
-            	      String line;
-            	      while ((line = in.readLine()) != null) {
-            	         Log.e(TAG, line);
-            	      }
-            	      proc.waitFor();
-            	      in.close();
-            	      out.close();
-            	      proc.destroy();
-            	   }
-            	   catch (Exception e) {
-            	      e.printStackTrace();
+//            		   Log.e(TAG, "try read");
+            	       String line;
+            	       while ((line = in.readLine()) != null) {
+            	    	   Log.e(TAG, line);
+            	       }
+            	       proc.waitFor();
+            	       in.close();
+            	       out.close();
+            	       proc.destroy();
+
+            	       if(proc.exitValue() == 1){
+            	    	   Log.w(TAG, "proc.exitValue() : 	" + proc.exitValue());
+            	    	   Log.w(TAG, "setRoot2 fail, starting setBlazeRoot");
+            	    	   setBlazeRoot(filename);
+            	       }
+            	   }catch (Exception e) {
+            		   e.printStackTrace();
+
             	   }
             	}
             }
 
         });
+	}
+
+	private void setBlazeRoot(final String filename){
+		Process proc = null;
+		BufferedReader in = null;
+ 	    PrintWriter out = null;
+		try {
+//	    	   Log.e(TAG, "trying execution again");
+	    	   proc = Runtime.getRuntime().exec("chmod 744 " + filename, null, new File("/data/data/com.bithedge.scriptroot/files/"));
+	    	   proc = Runtime.getRuntime().exec("ls", null, new File("/data/data/com.bithedge.scriptroot/files/"));
+	    	   Log.i(TAG, "executing file  :" + filename);
+//		   	   proc = Runtime.getRuntime().exec(EXC_LABEL + filename, null, new File("/data/data/com.bithedge.scriptroot/files/"));
+	    	   proc = Runtime.getRuntime().exec("sh " + filename, null, new File("/data/data/com.bithedge.scriptroot/files/"));
+		       in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+		       out = new PrintWriter(proc.getOutputStream(), true);
+		       String line;
+		       while ((line = in.readLine()) != null) {
+		    	   Log.e(TAG, line);
+		       }
+	  	       proc.waitFor();
+	  	       in.close();
+	  	       out.close();
+//	  	       proc.destroy();
+		 }catch (IOException e1) {
+				// TODO Auto-generated catch block
+		    	e1.printStackTrace();
+		 } catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+		  		e1.printStackTrace();
+		 }
 	}
 	/**
 	 * 	start here http://stackoverflow.com/questions/9386972/interactive-commands-using-java-runtime-getruntime-exec
